ad9b42dc891b791c9704b0294c273fac9c638840
Chani Mores <chani.mores@gmail.com>
2023-01-07 14:03:25 -0500

    Add laser prefab

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lasers : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     bool poweredOn = true;
 
