2b0ee407841bb6bd092d58fcda673dcc260f0a70
Chani Mores <chani.mores@gmail.com>
2023-01-07 13:58:32 -0500

    Make room 3 puzzle work

--- Assets/Scripts/Lasers.cs
+++ Assets/Scripts/Lasers.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Lasers : MonoBehaviour
 {
+    bool poweredOn = true;
+
     void OnTriggerEnter2D(Collider2D otherObj) {
         if (otherObj.gameObject.tag == "Player") {
+            Debug.Log("ded");
             // dies
         }
+    }
+
+    void TurnOffLaser()
+    {
+        this.poweredOn = false;
+        this.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
