a8f29477f8c53256b752f2f9d24b782c2a8831b9
Audrey Guo <84641452+guowiththeflow@users.noreply.github.com>
2023-01-08 17:08:37 -0500

    Some stuff with title screen

---
+++ Assets/ControlPanelScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+
+        }
+    }
+}
