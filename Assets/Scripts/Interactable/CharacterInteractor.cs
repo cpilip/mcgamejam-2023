adad1451597573b852b40c93a9ce2f40bfa080c7
cpilip <pilipchristina@gmail.com>
2023-01-19 15:33:33 -0500

    Fix needing to re-enter wireboxes

--- Assets/Scripts/Interactable/CharacterInteractor.cs
+++ Assets/Scripts/Interactable/CharacterInteractor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterInteractor : MonoBehaviour
 {
     [SerializeField] private GameObject m_current;
+    [SerializeField] private GameObject m_currentWirebox;
 
     void Awake()
     {
@@ -21,6 +22,16 @@ public class CharacterInteractor : MonoBehaviour
             StartCoroutine(LetAnimationPlay());
 
         }
+
+        // Fix for re-entering wireboxes - to not introduce more bugs, we'll have to leave
+        // m_current as is, but we know it's null when entering a wirebox since the tag is Dashable,
+        // not Interactable; we'll exploit this as a flag
+        if (Input.GetButtonDown("interact") && m_current == null && m_currentWirebox && m_currentWirebox.tag == "Interactable")
+        {
+            m_currentWirebox.SendMessage("InteractWith");
+            RatAnimator.Instance.TryInteract();
+            StartCoroutine(LetAnimationPlay());
+        }
     }
     
     void OnTriggerEnter2D(Collider2D other)
@@ -33,6 +44,14 @@ public class CharacterInteractor : MonoBehaviour
             m_current = other.gameObject;
 
         }
+
+        // Fix for re-entering wireboxes - we'll secretly track the box when it still has the Dashable 
+        // tag in the CharacterInteractor
+        if (other.CompareTag("Dashable") && other.gameObject.name.Contains("DashableWiresBoxTrigger"))
+        {
+            Debug.Log("Setting wirebox");
+            m_currentWirebox = other.gameObject;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -42,6 +61,16 @@ public class CharacterInteractor : MonoBehaviour
             if (other.gameObject.Equals(m_current))
             {
                 m_current = null;
+            }
+        }
+
+        // Fix for re-entering wireboxes
+        if (other.CompareTag("Interactable") && other.gameObject.name.Contains("DashableWiresBoxTrigger"))
+        {
+            if (other.gameObject.Equals(m_currentWirebox))
+            {
+                Debug.Log("Setting wirebox");
+                m_currentWirebox = null;
             }
         }
     }
